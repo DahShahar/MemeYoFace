11bced598e49b935a92885b1851fb9989da29447
Alexander Karle <akarle@umass.edu>
2016-02-26 22:23:12 -0500

    key fix path

--- frontend/js/indicoBasic.js
+++ frontend/js/indicoBasic.js
@@ -3,11 +3,12 @@ var fs = require('fs');
 var express = require("express");
 var app = express();
 
-var key = JSON.parse(fs.readFileSync('indico/key.json', 'utf8'))[0]
+var key = JSON.parse(fs.readFileSync('../../indico/key.json', 'utf8'))[0]
 indico.apiKey = key;
 fs.readFile(, function(err, data) {
   base64data = new Buffer(data).toString('base64');
 });
+
 indico.fer(filename, base64data)
   .then(function(res) {
     console.log(res);
