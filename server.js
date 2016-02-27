b50b517a16ad0ceab737fc31768dc3c1ade4669c
Alexander Karle <akarle@umass.edu>
2016-02-26 21:49:48 -0500

    Indico Key

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+var fs = require('fs');
 var express = require("express");
 var app = express();
 app.use(express.static("public"));
 app.listen(3000,function(){
   console.log("server start");
 });
+//var key = JSON.parse(fs.readFileSync('indico/key.json', 'utf8'))[0]
